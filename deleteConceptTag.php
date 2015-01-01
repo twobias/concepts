511908ddb995f42a237cd6795e2ef17fa3a2e707
root <twobias@gmail.com>
2015-01-01 17:25:45 +0100

    Fix deletion of tags - resolves #19

--- deleteConceptTag.php
+++ deleteConceptTag.php
@@ -7,12 +7,12 @@
   
   if ($tagid > 0) {
     /* create a prepared statement */
-    if ($stmt = mysqli_prepare($con, "DELETE FROM concepttag WHERE tagid=? AND conceptid = ?")) {
+    if ($stmt = mysqli_prepare($con, "DELETE FROM concepttags WHERE tagid=? AND conceptid = ?")) {
       mysqli_stmt_bind_param($stmt, "ii", $tagid, $conceptid);
       mysqli_stmt_execute($stmt);
-      $sql = mysqli_stmt_get_result($stmt);
+      $result = mysqli_stmt_get_result($stmt);
     } else {
-      echo "Error: " . $stmt;
+      echo "Error: " . $result;
     }
   } else {
     echo "error id = " . $id;
